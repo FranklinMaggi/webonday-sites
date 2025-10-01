8d66130b5badba11a68d1315111b8cb4be8b4fd9
Francesco Maggi <franklin19@outlook.it>
2025-10-01 11:56:59 +0200

    Eliminazione app.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// sostituisci 'nome-repo' col nome effettivo della repo su GitHub
 export default defineConfig({
+  base: '/nome-repo/',
   plugins: [react()],
-})
+});
